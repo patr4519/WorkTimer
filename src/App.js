30e35961fb0f25ab1ed4586bb1afed9b0624846b
patr4519 <jero96@mail.ru>
2023-02-28 22:57:47 +0300

    +

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
 
 const CountDown = ({ hours = 0, minutes = 0, seconds = 0 }) => {
   const [paused, setPaused] = React.useState(true);
+
   let [[h, m, s], setTime] = React.useState([hours, minutes, seconds]);
 
   const tick = () => {
@@ -30,24 +31,49 @@ const CountDown = ({ hours = 0, minutes = 0, seconds = 0 }) => {
   };
 
   React.useEffect(() => {
-    const timerID = setInterval(() => tick(), 1000);
+    const timerID = setInterval(() => {
+      tick();
+    }, 1000);
     return () => clearInterval(timerID);
   });
 
   return (
     <>
-    <h1>Working time</h1>
-    <div>
-      <p>{`${h.toString().padStart(2, "0")}:${m.toString().padStart(2, "0")}:${s
-        .toString()
-        .padStart(2, "0")}`}</p>
-      <button onClick={() => setPaused(!paused)}>
-        {paused ? "Start" : "Pause"}
-      </button>
-      <button onClick={() => reset()}>Reset</button>
-    </div>
+      <h1>Working time</h1>
+      <div>
+        <p>{`${h.toString().padStart(2, "0")}:${m
+          .toString()
+          .padStart(2, "0")}:${s.toString().padStart(2, "0")}`}</p>
+        <button onClick={() => setPaused(!paused)}>
+          {paused ? "Start" : "Pause"}
+        </button>
+        <button onClick={() => reset()}>Reset</button>
+      </div>
     </>
   );
 };
 
 export default App;
+
+// import React, { useState, useEffect } from 'react';
+
+// const IntervalExample = () => {
+//   const [seconds, setSeconds] = useState(0);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setSeconds(seconds => seconds + 1);
+//     }, 1000);
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         {seconds} seconds have elapsed since mounting.
+//       </header>
+//     </div>
+//   );
+// };
+
+// export default IntervalExample;
